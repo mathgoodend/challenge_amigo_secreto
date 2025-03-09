8a91b835b5600c004e837465b7eec60bf661f619
Matheus Bonfim <matheussobonfim@outlook.com>
2025-03-09 14:08:04 -0300

    Inicializacao do array, criacao das funcoes que adicionam amigos a lista e atualizam ela na tela. Uso de console.log para testes

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const amigos = [];
+console.log(lista);
+
+function adicionarAmigo() {
+    const amigo = document.getElementById('amigo');
+    if (amigo.value.length == 0 || amigo.value.trim() == "") {
+        amigo.value = "";
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(amigo.value);
+        amigo.value = "";
+    }
+    console.log(amigos);
+    atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    const e = document.createElement('li');
+
+    lista.innerHTML = "";
+    console.log(lista);
+    for (let i = 0; i < amigos.length; i++) {
+        e.innerHTML = amigos[i];
+        lista.appendChild(e);
+    }
+    console.log(lista);
+}
